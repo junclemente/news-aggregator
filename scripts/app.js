28fb83a83eb6416087b62b058acfc4076304ee2d
Jun Clemente <kjstudios858@gmail.com>
2017-01-18 12:21:27 -0800

    removed app.js:281

--- scripts/app.js
+++ scripts/app.js
@@ -277,7 +277,7 @@ APP.Main = (function() {
       score.style.lineHeight = (scale * 40) + 'px';
 
       // Now figure out how wide it is and use that to saturate it.
-      scoreLocation = score.getBoundingClientRect();
+      // scoreLocation = score.getBoundingClientRect();
       var saturation = (100 * ((scoreLocation.width - 38) / 2));
 
       score.style.backgroundColor = 'hsl(42, ' + saturation + '%, 50%)';
